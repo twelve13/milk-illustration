e0eeb2137618e2d474dc5c74f71dd7742199fd21
Caroline Chen <carolinecaroline@gmail.com>
2018-08-17 11:05:10 -0400

    animate

---
+++ script.js
@@ -0,0 +1,14 @@
+const divs = document.getElementsByTagName("div");
+const button = document.getElementById("button-container");
+const blade = document.getElementById("blade");
+
+function show(){
+	button.remove();
+	setTimeout(function(){blade.play()}, 25700);
+	for(let i=0; i<divs.length; i++){
+		setTimeout(function(){divs[i].classList.add("showme")}, i*200);
+	}
+	
+}
+
+button.addEventListener("click", show);
